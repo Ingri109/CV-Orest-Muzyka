4bda3e47be5898a4d0acd9437113f4bd5e240c69
Ingri109 <stalker.orest.109@gmail.com>
2024-08-21 15:00:05 +0200

    test-0.1.5

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    distDir: 'out',
+    // // output: 'export',
+    // distDir: 'out',
 };
 
 export default withNextIntl(nextConfig);
